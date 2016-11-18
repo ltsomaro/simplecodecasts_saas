de623d53fc149ee59d685dc3545e83c871912cb0
Louis Tsomaropoulos <louist@bigpond.net.au>
2016-11-18 05:37:29 +0000

    Added create profile linke.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
     def new
-        
+        # form where a user can complete profile
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile
     end
     
     def update
