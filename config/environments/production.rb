d7726f8af8615f678e452448409fa70560490e69
Louis Tsomaropoulos <louist@bigpond.net.au>
2016-11-29 21:59:33 +0000

    Added default mailer url options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'heroku.com' }
+  
+  config.action_mailer.default_url_options = { :host => 'nameless-castle-73653.heroku.com' }
+  
 end
