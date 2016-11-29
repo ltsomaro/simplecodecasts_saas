c688b5ff6bdf8da9f1514b479e3dc5330942c69c
Louis Tsomaropoulos <louist@bigpond.net.au>
2016-11-29 21:29:47 +0000

    Various changes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
@@ -34,4 +35,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+ 
+  
 end
